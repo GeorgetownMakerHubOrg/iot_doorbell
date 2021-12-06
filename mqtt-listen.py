0a0e89583b0f3a9f685aca2953e702fc6e672ae2
Pascal Girard <pascal.girard@me.com>
2021-12-06 14:13:50 -0500

    Working mqtt subscription for doorbell!
    It rings!

--- mqtt-listen.py
+++ mqtt-listen.py
@@ -1,41 +1,38 @@
-import random, time, json, requests, os
-import config 
+# GNU General Public License <https://www.gnu.org/licenses>
+# Pascal Girard pascal.girard@georgetown.edu
+import random, time, json, os
+import paho.mqtt.client as mqtt
 
-from paho.mqtt import client as mqtt_client
+# Adafruit parameters
+X_AIO_KEY = "aio_ouajbiadfad8fasdasdfasdfVOZ5"                       # Adafruit IO key
+USER = "adafruit_user"                                               # Adafruit user
+MQTT_SERVER = "io.adafruit.com"
+MQTT_USER = USER
+MQTT_PASSWD = X_AIO_KEY
+MQTT_TOPIC = "gumakerhub/feeds/makerhubevents.backdoorbell"
 
-broker = config.MQTT_SERVER
-port = 1883
-topic = config.MQTT_SUB_TOPIC
 # generate client ID with random suffix
-client_id = "doorbell-mqtt-test-"+str(random.getrandbits(16))
-username = config.MQTT_USER
-password = config.MQTT_PASSWD
+username = MQTT_USER
+password = MQTT_PASSWD
 
-# Reasonable default values
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected with result code {rc}")
+    # reconnect after losing the connection with the broker, it will continue to subscribe to the raspberry/topic topic
+    client.subscribe(MQTT_TOPIC, qos=1)
 
-data = {"count": 0}
+# the callback function, it will be triggered when receiving messages
+def on_message(client, userdata, msg):
+    print('Payload is:', json.loads(msg.payload.decode()))
+    print('Knock-knock')
+    os.system("/home/pi/iot_doorbell/knock")
 
-def connect_mqtt() -> mqtt_client:
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-    client = mqtt_client.Client(client_id)
-    client.username_pw_set(username, password)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
+client = mqtt.Client()
+client.username_pw_set(username, password)
+client.on_connect = on_connect
+client.on_message = on_message
 
-def subscribe(client: mqtt_client):
-    def on_message(client, userdata, msg):
-        global data
-        print('Payload is:', json.loads(msg.payload.decode()))
-        data['count'] += 1
-        os.system("/home/pi/iot_doorbell/knock")
-    client.subscribe(topic)
-    client.on_message = on_message
-print("client id is: ", client_id)
-client = connect_mqtt()
-subscribe(client)
+# Connect with  broker address, broker port number, and keep-alive time respectively
+client.connect(MQTT_SERVER, 1883, 60)
+
+# Network loop blocking, it will not actively end the program before calling disconnect() or the program crash
 client.loop_forever()
